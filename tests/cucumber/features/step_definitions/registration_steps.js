74727f0d9ae15d4643661743e58ec6e8442ec315
Aaron Malone <aaronmalone@gmail.com>
2015-05-25 11:17:01 -0400

    Cleaned up code

--- tests/cucumber/features/step_definitions/registration_steps.js
+++ tests/cucumber/features/step_definitions/registration_steps.js
@@ -9,47 +9,26 @@
         var url = require('url');
 
         this.Given(/^I am an unauthenticated User$/, function () {
-
-            console.log('I am an unauthenticated User')
-
             // no callbacks! DDP has been promisified so you can just return it
             return this.server.call('reset'); // this.ddp is a connection to the mirror
         });
 
         this.When(/^I navigate to the "(.*)"$/, function (relativePath, callback) {
-
-            // helper.world.browser.url(url.resolve(helper.world.mirrorUrl, relativePath)).call(callback);
-
-            // console.log(">>>>> " + relativePath)
-
             //// WebdriverIO supports Promises/A+ out the box, so you can return that too
-            this.client. // this.browser is a pre-configured WebdriverIO + PhantomJS instance
-                url(url.resolve(process.env.ROOT_URL, relativePath)). // process.env.ROOT_URL always points to the mirror
-                call(callback);
+            this.client // this.browser is a pre-configured WebdriverIO + PhantomJS instance
+                .url(url.resolve(process.env.ROOT_URL, relativePath)) // process.env.ROOT_URL points to the mirror
+                .call(callback);
         });
 
         this.When(/^I click the "([^"]*)" link$/, function (linkClass, callback) {
-            var cli = this.client;
-
             this.client.waitForVisible('body *').click(linkClass, callback);
-
-            //this.client.waitForVisible('body *').isExisting('#login-dropdown-list', function(err, isExisting) {
-            //    if(!isExisting) {
-            //        console.log("login-link-text not existing")
-            //        cli.click('.login-link-textASDFASDF', function() {})
-            //        console.log("post click")
-            //        cli.getHTML('.login-link-text').should.exist.and.notify(callback)
-            //    }
-            //})
         });
 
         this.Then(/^I should see the title "([^"]*)"$/, function (arg1, callback) {
-            // Write code here that turns the phrase above into concrete actions
             this.client.call(callback);
         });
 
         this.Then(/^I should see the "([^"]*)" element$/, function (arg1, callback) {
-            // Write code here that turns the phrase above into concrete actions
             this.client.call(callback);
         });
 
@@ -58,7 +37,6 @@
         });
 
         this.Then(/^I should not be able to see content that requires Authentication$/, function (callback) {
-            var cli = this.client;
             this.client.isExisting('#protected', function(err, isExisting) {
                 isExisting.should.be.false
             }).call(callback);
@@ -68,7 +46,6 @@
         // Registration
 
         this.When(/^I register for the first time$/, function (callback) {
-
             this.client.setValue('#login-email', 'test123@foo.com')
                 .setValue('#login-password', 'yadayadayaday')
                 .click('#login-buttons-password')
