7393fa5858f952d0c77ec10ac76833e127111902
Aaron Malone <aaronmalone@gmail.com>
2015-05-27 12:59:54 -0400

    Moved client js code to separate directory

--- ducking.js
+++ ducking.js
@@ -1,33 +1,5 @@
 if (Meteor.isClient) {
-
-    function getCurrentUserEmail() {
-        var currentUser = Meteor.user();
-        var emailsArray = currentUser.emails;
-        var firstEmail = emailsArray[0];
-        return firstEmail.address;
-    }
-
-    Template.skills.helpers({
-        hasSkills: function () {
-            var skillService = new SkillService();
-            var skillsArray = skillService.getSkills(getCurrentUserEmail());
-            return skillsArray.length > 0;
-        },
-
-        getSkills: function () {
-            var skillService = new SkillService();
-            return skillService.getSkills(getCurrentUserEmail());
-        }
-    });
-
-    Template.skillEntry.events({
-        'click button#add-skill-button': function (evt) {
-            evt.preventDefault();
-            var skill = Template.instance().$('#add-skill-input').val();
-            var skillService = new SkillService();
-            skillService.addSkill(getCurrentUserEmail(), skill, false);
-        }
-    });
+    
 }
 
 if (Meteor.isServer) {
